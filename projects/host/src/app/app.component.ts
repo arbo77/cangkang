b68f4c25039d7c9bb600d01a2b5b5156f1f62dea
Ari <arbo77@gmail.com>
2023-06-17 23:36:01 +0700

    add dexie

--- projects/host/src/app/app.component.ts
+++ projects/host/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Dexie, { Table, liveQuery } from 'dexie';
+
+export interface TodoList {
+  id?: number;
+  title: string;
+}
+
+class DB extends Dexie {
+  todoLists!: Table<TodoList, number>;
+  constructor() {
+    super('federate');
+
+    this.version(3).stores({
+      todoLists: '++id',
+      todoItems: '++id, todoListId',
+    });
+  }
+}
+
+export const db = new DB();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'host';
+  dataCount: number = 0;
+
+  todoLists$ = liveQuery(() => db.todoLists.toArray());
+
+  ngOnInit() {
+    localStorage.setItem('shared', 'hello');
+
+    this.todoLists$.subscribe((items: TodoList[]) => {
+      this.dataCount = items.length;
+    });
+  }
 }
--- projects/welcome/src/app/welcome/welcome.component.ts
+++ projects/welcome/src/app/welcome/welcome.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import Dexie, { Table, liveQuery } from 'dexie';
+
+export interface TodoList {
+  id?: number;
+  title: string;
+}
+
+class DB extends Dexie {
+  todoLists!: Table<TodoList, number>;
+  constructor() {
+    super('federate');
+
+    this.version(3).stores({
+      todoLists: '++id',
+      todoItems: '++id, todoListId',
+    });
+  }
+}
+export const db = new DB();
 
 @Component({
   selector: 'app-welcome',
@@ -7,7 +26,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
   title = 'welcome';
-  constructor() {}
+  dataCount: number = 0;
+  todoLists$ = liveQuery(() => db.todoLists.toArray());
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.todoLists$.subscribe((items: TodoList[]) => {
+      this.dataCount = items.length;
+      console.log(items.length);
+    });
+  }
+
+  getStorage() {
+    this.title = localStorage.getItem('shared') || 'nothing';
+    db.todoLists.add({
+      title: this.title,
+    });
+  }
 }
